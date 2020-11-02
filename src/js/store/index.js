4334e7bb39875f1ab3d5a1fa51539c595f7c050e
Jason Racey <jasonracey@gmail.com>
2020-11-01 20:30:03 -0800

    Added api call saga

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,20 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
+import createSagaMiddleware from "redux-saga";
+import apiSaga from "../sagas/api-saga";
+
+const initializeSagaMiddleware = createSagaMiddleware();
+
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    applyMiddleware(forbiddenWordsMiddleware)
+    storeEnhancers(
+        applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
+    )
 );
+
+initializeSagaMiddleware.run(apiSaga);
 
 export default store;
