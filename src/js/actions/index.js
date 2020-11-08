24925f3546307292b7b3aa8907627f0227378afa
Jason Racey <jasonracey@gmail.com>
2020-11-07 19:56:41 -0800

    Fetch posts using redux-thunk

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,6 +4,27 @@ export function addArticle(payload) {
     return { type: ADD_ARTICLE, payload };
 }
 
+//"Error: Actions must be plain objects. Use custom middleware 
+//for async actions". We cannot call Fetch from within an action 
+//creator in Redux.
+// export function getData() {
+//     return fetch("https://jsonplaceholder.typicode.com/posts")
+//         .then(response => response.json())
+//         .then(json => {
+//             return { type: "DATA_LOADED", payload: json }
+//         });
+// }
+
 export function getData() {
-    return { type: "DATA_REQUESTED" };
-}
+    return function(dispatch) {
+      return fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(json => {
+          dispatch({ type: "DATA_LOADED", payload: json });
+        });
+    };
+  }
+
+// export function getData() {
+//     return { type: "DATA_REQUESTED" };
+// }
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,20 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/index";
 import { forbiddenWordsMiddleware } from "../middleware";
-import createSagaMiddleware from "redux-saga";
-import apiSaga from "../sagas/api-saga";
+import thunk from "redux-thunk";
+//import createSagaMiddleware from "redux-saga";
+//import apiSaga from "../sagas/api-saga";
 
-const initializeSagaMiddleware = createSagaMiddleware();
+//const initializeSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     rootReducer,
-    storeEnhancers(
-        applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
-    )
+    storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+    // storeEnhancers(
+    //     applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
+    // )
 );
 
-initializeSagaMiddleware.run(apiSaga);
+//initializeSagaMiddleware.run(apiSaga);
 
 export default store;
